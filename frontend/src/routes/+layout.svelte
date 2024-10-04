8043cb5bbe58803b01c6b7d51c69d3d16218014a
Jason Rasmussen <jason@rasm.me>
2024-10-04 12:45:07 -0400

    chore: better logging

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import '$lib/app.css';
   import { backendUrl } from '$lib/environment';
 
-  console.log(`Backend URL: ${backendUrl}`);
+  console.log(`Using Backend URL: ${backendUrl}`);
 </script>
 
 <section class="h-full flex flex-col align-center gap-2 max-w-screen-lg mx-auto bg-white pb-36">
