595f2c87424002004b08f05eb72bdfc553d6603a
Zack Pollard <zackpollard@ymail.com>
2024-10-11 16:58:50 +0100

    ci: align dev environment domains with monitoring stack

--- deployment/modules/cloudflare/frontend/locals.tf
+++ deployment/modules/cloudflare/frontend/locals.tf
@@ -2,5 +2,5 @@ locals {
   domain_env    = var.env == "prod" ? "" : "${var.env}."
   domain_stage  = var.stage != "" ? "${var.stage}." : ""
   domain_prefix = "${local.domain_stage}${local.domain_env}"
-  domain        = "${local.domain_prefix}data.immich.app"
+  domain        = "data.${local.domain_prefix}immich.app"
 }
