5585f9b1da12e989c466e9cb7a8676831875560a
Zack Pollard <zackpollard@ymail.com>
2025-01-15 15:59:56 +0000

    chore: api return data from all-time

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,6 +1,10 @@
+import { DateTime } from 'luxon';
+
 export type GithubRepo = { name: string; organization: string };
 export type GithubUser = { login: string; id: number };
 
 export const GithubRepos: { [key: string]: GithubRepo } = {
   IMMICH: { name: 'immich', organization: 'immich-app' },
 };
+
+export const IMMICH_INCEPTION = DateTime.fromISO('2022-02-01T00:00:00Z');
