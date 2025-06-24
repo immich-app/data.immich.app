33e39c7194b8ee20c4576fac380883ed68e58d3c
Daniel Dietzler <mail@ddietzler.dev>
2025-06-24 22:29:06 +0200

    chore: migrate to tailwind v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 const apiHost = process.env.IMMICH_BACKEND || 'https://data.immich.app/';
 
 console.log(`Connecting to ${apiHost}`);
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [tailwindcss(), sveltekit()],
   server: {
     proxy: {
       '/api': {
