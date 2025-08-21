1b8004ceb3509ef5206204b72eda956dee9820eb
Jason Rasmussen <jason@rasm.me>
2025-08-21 09:37:30 -0400

    feat: update footer (#174)

--- frontend/src/lib/components/layout/Footer.svelte
+++ frontend/src/lib/components/layout/Footer.svelte
@@ -1,40 +1,30 @@
 <script lang="ts">
-  import { Card, Heading, HStack, Icon, Link, Stack, Text, ThemeSwitcher, VStack } from '@immich/ui';
-  import {
-    mdiAndroid,
-    mdiApple,
-    mdiGithub,
-    mdiKey,
-    mdiMessageOutline,
-    mdiOfficeBuildingOutline,
-    mdiReddit,
-    mdiServerOutline,
-    mdiShoppingOutline,
-    mdiWeb,
-  } from '@mdi/js';
+  import { Heading, HStack, Icon, Link, Stack, Text, ThemeSwitcher, VStack } from '@immich/ui';
+  import { mdiCubeOutline, mdiKey, mdiOfficeBuildingOutline, mdiServerOutline, mdiShoppingOutline } from '@mdi/js';
+  import { siAndroid, siApple, siDiscord, siGithub, siImmich, siReddit } from 'simple-icons';
 </script>
 
 <div class="bg-dark/10 rounded-t-3xl mt-16 p-8">
   <div class="max-w-(--breakpoint-lg) mx-auto xl:py-8">
     <Stack gap={8}>
-      <div class="grid grid-cols-2 gap-8 place-center lg:grid-cols-5">
+      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 place-center">
         <Stack>
           <Heading size="tiny">Social</Heading>
           <Link href="https://github.com/immich-app/immich" target="_blank" rel="noopener noreferrer">
             <HStack>
-              <Icon icon={mdiGithub} size="1.5em" />
+              <Icon icon={siGithub} size="1.5em" />
               <Text>GitHub</Text>
             </HStack>
           </Link>
           <Link href="https://discord.immich.app/" target="_blank" rel="noopener noreferrer">
             <HStack>
-              <Icon icon={mdiMessageOutline} size="1.5em" />
+              <Icon icon={siDiscord} size="1.5em" />
               <Text>Discord</Text>
             </HStack>
           </Link>
           <Link href="https://www.reddit.com/r/immich/" target="_blank" rel="noopener noreferrer">
             <HStack>
-              <Icon icon={mdiReddit} size="1.5em" />
+              <Icon icon={siReddit.path} size="1.5em" />
               <Text>Reddit</Text>
             </HStack>
           </Link>
@@ -48,13 +38,13 @@
             rel="noopener noreferrer"
           >
             <HStack>
-              <Icon icon={mdiAndroid} size="1.5em" />
+              <Icon icon={siAndroid.path} size="1.5em" />
               <Text>Android</Text>
             </HStack>
           </Link>
           <Link href="https://apps.apple.com/us/app/immich/id1613945652" target="_blank" rel="noopener noreferrer">
             <HStack>
-              <Icon icon={mdiApple} size="1.5em" />
+              <Icon icon={siApple.path} size="1.5em" />
               <Text>iOS</Text>
             </HStack>
           </Link>
@@ -62,6 +52,28 @@
             <HStack>
               <Icon icon={mdiServerOutline} size="1.5em" />
               <Text>Server</Text>
+            </HStack>
+          </Link>
+        </Stack>
+
+        <Stack>
+          <Heading size="tiny">Sites</Heading>
+          <Link href="https://immich.app" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={siImmich} size="1.5em" />
+              <Text>Documentation</Text>
+            </HStack>
+          </Link>
+          <Link href="https://my.immich.app" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={siImmich} size="1.5em" />
+              <Text>My Immich</Text>
+            </HStack>
+          </Link>
+          <Link href="https://api.immich.app" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiCubeOutline} size="1.5em" />
+              <Text>Immich API</Text>
             </HStack>
           </Link>
         </Stack>
@@ -83,22 +95,6 @@
         </Stack>
 
         <Stack>
-          <Heading size="tiny">Sites</Heading>
-          <Link href="https://immich.app" target="_blank" rel="noopener noreferrer">
-            <HStack>
-              <Icon icon={mdiWeb} size="1.5em" />
-              <Text>Documentation</Text>
-            </HStack>
-          </Link>
-          <Link href="https://my.immich.app" target="_blank" rel="noopener noreferrer">
-            <HStack>
-              <Icon icon={mdiWeb} size="1.5em" />
-              <Text>My Immich</Text>
-            </HStack>
-          </Link>
-        </Stack>
-
-        <Stack>
           <Heading size="tiny">Company</Heading>
           <Link href="https://futo.org/" target="_blank" rel="noopener noreferrer">
             <HStack>
@@ -108,9 +104,6 @@
           </Link>
         </Stack>
       </div>
-      <Card>
-        <img src="/img/social-preview.webp" alt="Social Preview" />
-      </Card>
       <VStack class="text-center">
         <Text size="large">This project is available under GNU AGPL v3 license.</Text>
         <Text color="muted" variant="italic">Privacy should not be a luxury</Text>
